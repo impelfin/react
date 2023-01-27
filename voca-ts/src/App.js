e09ac818b6ed908c668146f469c576d9d7ed696e
JunHyen Mun <impelfin@gmail.com>
2023-01-27 16:38:58 +0900

    voca-ts

---
+++ voca-ts/src/App.js
@@ -0,0 +1,26 @@
+import Day from './component/Day';
+import DayList from './component/DayList';
+import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />}></Route>
+          <Route path="/day/:day" element={<Day />}></Route>
+          <Route path="/create_word" element={<CreateWord />}></Route>
+          <Route path="/create_day" element={<CreateDay />}></Route>
+          <Route element={<EmptyPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

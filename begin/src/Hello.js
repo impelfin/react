16790592f6af4c5a090625a7336bb935802cd925
JunHyen Mun <impelfin@gmail.com>
2023-01-30 16:57:25 +0900

    begin

--- begin/src/Hello.js
+++ begin/src/Hello.js
@@ -1,5 +1,14 @@
-import React from 'react'
-
-function Hello() {
-    reutrn <div>안녕하세요</dev>
+function Hello({ name, color, isSpecial }) {
+  return (
+    <div style={{ color }}>
+      {isSpecial && <b>*</b>}
+      안녕하세요 {name}
+    </div>
+  );
 }
+
+Hello.defaultProps = {
+  name: '이름없음',
+};
+
+export default Hello;
